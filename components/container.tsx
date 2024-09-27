cbacd83f20abeeb1abac0913b490b1fd5c08db9c
Andcool-Systems <drolll2007@gmail.com>
2024-09-27 20:35:46 +0300

    fixed height

--- components/container.tsx
+++ components/container.tsx
@@ -228,7 +228,7 @@ export const container = (
 			<svg
 				xmlns="http://www.w3.org/2000/svg"
 				width={width}
-				height={height + 50}
+				height={height + 2}
 			>
 				<style>${style}</style>
 				<rect
